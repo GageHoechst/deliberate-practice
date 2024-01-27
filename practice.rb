8d146822b9f70e9fb839635d229d7b468309c352
GageHoechst <hoechstg99@gmail.com>
2024-01-27 10:16:30 -0500

    Update practice.rb

--- practice.rb
+++ practice.rb
@@ -8,3 +8,19 @@ puts color.upcase
 x = 6
 y = 4
 puts x + y
+# Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+number = 7
+if number == 10
+    puts 0
+else puts -1
+end
+# Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10,
+ # and prints 0 if the number is equal to 10.
+ number = 10
+ if number < 10
+    puts -1
+ elsif number > 10
+    puts 1
+ else
+    puts 0
+ end
