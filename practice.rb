6137a9e2e4408a7712c8c5f790208c51c0b4b06a
GageHoechst <hoechstg99@gmail.com>
2024-01-27 10:05:30 -0500

    Add exercise solutions

---
+++ practice.rb
@@ -0,0 +1,10 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+x = 5
+puts x + 10
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+color = "red"
+puts color.upcase
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+x = 6
+y = 4
+puts x + y
