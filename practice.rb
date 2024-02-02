73e0cd64d5f8594c59cf9196172d6df3e75cd42f
GageHoechst <hoechstg99@gmail.com>
2024-02-01 19:25:55 -0500

    Update practice.rb

--- practice.rb
+++ practice.rb
@@ -1,55 +1,61 @@
 # Write a program that uses a variable to store a number, then prints that number plus 10.
-x = 5
-puts x + 10
+# x = 5
+# puts x + 10
 # Write a program that uses a variable to store a word, then prints that word with all capital letters.
-color = "red"
-puts color.upcase
+# color = "red"
+# puts color.upcase
 # Write a program that uses variables to store two numbers, then prints the numbers added together.
-x = 6
-y = 4
-puts x + y
+# x = 6
+# y = 4
+# puts x + y
 # Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
-number = 7
-if number == 10
-    puts 0
-else puts -1
-end
+# number = 7
+# if number == 10
+#     puts 0
+# else puts -1
+# end
 # Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10,
  # and prints 0 if the number is equal to 10.
- number = 10
- if number < 10
-    puts -1
- elsif number > 10
-    puts 1
- else
-    puts 0
- end
+#  number = 10
+#  if number < 10
+#     puts -1
+#  elsif number > 10
+#     puts 1
+#  else
+#     puts 0
+#  end
  # Write a while loop to print the numbers 1 through 10.
-x = 0
-while x <= 10
-   puts x
-   x = x + 1
-end
+# x = 0
+# while x <= 10
+#    puts x
+#    x = x + 1
+# end
 # Write a while loop that prints the word "hello" 5 times.
- count = 0
- while count  < 5
-   puts "hello"
-   count = count + 1
- end
+#  count = 0
+#  while count  < 5
+#    puts "hello"
+#    count = count + 1
+#  end
  # Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
- while true
-   puts "enter a word:"
- word = gets.chomp
-  if word == "stop"
-   break
-  end
-end
+#  while true
+#    puts "enter a word:"
+#  word = gets.chomp
+#   if word == "stop"
+#    break
+#   end
+# end
 # Create an array to store 3 words. Then add two more words to the array and print the array on one line.
-colors = ["Red", "Blue", "Green"]
-colors.push("Purple")
-colors.push("Pink")
-pp colors
+# colors = ["Red", "Blue", "Green"]
+# colors.push("Purple")
+# colors.push("Pink")
+# pp colors
 # Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
-letters = ["a", "b", "c", "d"]
-letters[1] = 2
-pp letters
+# letters = ["a", "b", "c", "d"]
+# letters[1] = 2
+# pp letters
+# Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+# person = {"first name" => "Gage", "last name" => "Hoechst", "email adress" => "hoechstg99@gmail.com"}
+# pp person["first name"]
+# pp person["last name"]
+# pp person["email adress"]
+# Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
